922f21e48698d61136bdf0cf4e7cdd04fd5660b6
abrarimamsatria09 <092004abrar@gmail.com>
2025-05-22 15:34:27 +0700

    Membuat widget RecomendPlantCard

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constant.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
 import 'package:plant_app/screens/home/components/tittle_with_more_bbtn.dart';
 
@@ -14,13 +15,90 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          TittleWithMoreBtn(
-            title: "Recommended",
-            press: () {},
-          ),
+          TittleWithMoreBtn(title: "Recommended", press: () {}),
+          // it will cover 40% of our total height
+          RecomendPlantCard(size: size),
         ],
       ),
     );
   }
 }
 
+class RecomendPlantCard extends StatelessWidget {
+  const RecomendPlantCard({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+            ),
+            child: Image.asset(
+              "assets/images/kaktus.jpeg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(kDefaultPadding / 2),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 50,
+                  color: kPrimaryColor.withOpacity(0.23),
+                ),
+              ],
+            ),
+            child: Row(
+              children: <Widget>[
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Samantha\n".toUpperCase(),
+                        style: Theme.of(context).textTheme.labelLarge,
+                      ),
+                      TextSpan(
+                        text: "Russia".toUpperCase(),
+                        style: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  '\$440',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.labelLarge?.copyWith(color: kPrimaryColor),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
